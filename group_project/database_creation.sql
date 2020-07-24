c1f5defa4abcea25c432bb517f1dd4cea6d8bea8
Adam Wright <adam@adamjwright.com>
2020-07-24 03:19:45 -0700

    fix error msgs and add to statements

--- group_project/database_creation.sql
+++ group_project/database_creation.sql
@@ -1,5 +1,8 @@
+---------------------------------
+-- CS-340 Summer 2020: Group 34
+---------------------------------
 
--- Create table and insert data for Programmers
+-- Create table and insert data for Programmers -------------------------------
 DROP TABLE IF EXISTS `Programmers`;
 
 CREATE TABLE `Programmers` (
@@ -10,7 +13,8 @@ CREATE TABLE `Programmers` (
     `dateStarted` date, 
     `accessLevel` int(11),
     PRIMARY KEY (`programmerId`)
-)
+) ENGINE=InnoDB;
+
 
 LOCK TABLES `Programmers` WRITE;
 
@@ -21,7 +25,7 @@ INSERT INTO `Programmers` VALUES
 UNLOCK TABLES;
 
 
--- Create table and insert data for Companies
+-- Create table and insert data for Companies ---------------------------------
 DROP TABLE IF EXISTS `Companies`;
 
 CREATE TABLE `Companies` (
@@ -29,7 +33,7 @@ CREATE TABLE `Companies` (
     `companyName` varchar(255) NOT NULL,
     `dateJoined` date,
     PRIMARY KEY (`companyId`)
-);
+) ENGINE=InnoDB;
 
 LOCK TABLE `Companies` WRITE;
 
@@ -40,7 +44,7 @@ INSERT INTO `Companies` VALUES
 UNLOCK TABLES;
 
 
--- Create table and insert data for Projects
+-- Create table and insert data for Projects ----------------------------------
 DROP TABLE IF EXISTS `Projects`;
 
 CREATE TABLE `Projects` (
@@ -48,19 +52,60 @@ CREATE TABLE `Projects` (
     `projectName` varchar(255) NOT NULL, 
     `dateStarted` date,
     `lastUpdated` date, 
-    `inMaintenance` bit,
+    `inMaintenance` BOOLEAN DEFAULT TRUE,
     `companyId` int(11) NOT NULL,
+    PRIMARY KEY (`projectId`),
     FOREIGN KEY (`companyId`) REFERENCES `Companies` (`companyId`)
-);
+) ENGINE=InnoDB;
 
-LOCK TABLE `Projects` WRITE;
 
-INSERT INTO `Projects` (`projectName`, `dateStarted`, `lastUpdated`, `inMaintenance`, 'companyId') VALUES
-    ('Project 1', '2002-03-19', '2002-03-19', '1', (SELECT `companyId` FROM `Companies` WHERE `companyName` = 'Company 1')),
-    ('Project 2', '2020-02-27', '2020-05-08', '0', (SELECT `companyId` FROM `Companies` WHERE `companyName` = 'Company 2'))
-);
+LOCK TABLES `Projects` WRITE, `Companies` AS c WRITE, `Companies` AS c1 WRITE;
+
+INSERT INTO `Projects` (`projectName`, `dateStarted`, `lastUpdated`, `inMaintenance`, `companyId`) VALUES
+    ('Project 1', '2002-03-19', '2002-03-19', TRUE, (SELECT `companyId` FROM `Companies` AS c WHERE `companyName` = 'Company 1')),
+    ('Project 2', '2020-02-27', '2020-05-08', FALSE, (SELECT `companyId` FROM `Companies` AS c1 WHERE `companyName` = 'Company 2'));
 
 UNLOCK TABLES;
 
 
--- Create table and insert data for Bugs
+-- Create table and insert data for Bugs --------------------------------------
+DROP TABLE IF EXISTS `Bugs`;
+
+CREATE TABLE `Bugs` (
+`bugId` int(11) NOT NULL AUTO_INCREMENT,
+`projectId` int(11),
+`bugSummary` text NOT NULL,
+`bugDescription` text,
+`dateStarted` date NOT NULL,
+PRIMARY KEY (`bugId`),
+FOREIGN KEY (`projectId`) REFERENCES `Projects` (`projectId`)
+) ENGINE=InnoDB;
+
+
+LOCK TABLE `Bugs` WRITE, `Projects` WRITE;
+
+INSERT INTO `Bugs` (`projectId`, `bugSummary`, `bugDescription`, `dateStarted`) VALUES
+(),
+();
+
+UNLOCK TABLES;
+
+
+-- Create table and insert data for Bugs_Programmers --------------------------
+DROP TABLE IF EXISTS `Bugs_Programmers`;
+
+CREATE TABLE `Bugs` (
+`bugId` int(11) NOT NULL,
+`programmerId` int(11) NOT NULL,
+FOREIGN KEY (`bugId`) REFERENCES `Bugs` (`bugId`),
+FOREIGN KEY (`programmerId`) REFERENCES `Programmers` (`pogrammerId`)
+) ENGINE=InnoDB;
+
+
+LOCK TABLE `Bugs` WRITE, `Programmers` WRITE;
+
+INSERT INTO `Bugs_Programmers` (`bugId`, `programmerId`) VALUES
+(),
+();
+
+UNLOCK TABLES;
