18470b9e3364394e4c0294a0c20693c0baa881f9
hermancai <30858461+hermancai@users.noreply.github.com>
2020-08-05 20:16:47 -0700

    copy from doughnut chart branch

--- group_project/public/js/user-home.js
+++ group_project/public/js/user-home.js
@@ -485,3 +485,158 @@ function resetTable() {
         });
     }
 }
+
+
+/* FIXED BUGS DOUGHNUT CHART ----------------------------------------------- */
+
+// Function using D3.js to render a new doughnut chart with svg
+function printChart(fixedCount, brokenCount, countSize) {
+    // Set the dimensions and margins of the graph
+    let width = 300
+        height = 300
+        margin = 10
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    let radius = Math.min(width, height) / 2 - margin
+
+    // Append the svg object to the div called 'my_dataviz'
+    let svg = d3.select("#my_dataviz")
+        .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+        .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // Enter bug data
+    let data = {a: fixedCount, b: brokenCount}
+
+    // Set up graph text percentage in center
+    var centralText = svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr('font-size', '55px')
+        .attr('y', 19)
+        .style("opacity", 0.45);
+
+    // Create the central percentage
+    centralText.text(d3.format(".0%")(fixedCount / countSize));
+
+    // Set the graph color scale
+    let color = d3.scaleOrdinal()
+        .domain(data)
+        .range(["#004020", "#a50020"])      // Set colors for chart
+
+    // Compute the position of each group on the pie:
+    let pie = d3.pie()
+        .value(function(d) {return d.value; })
+    let data_ready = pie(d3.entries(data))
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+        .selectAll('whatever')
+        .data(data_ready)
+        .enter()
+        .append('path')
+        .attr('d', d3.arc()
+            .innerRadius(100)         // This is the size of the donut hole
+            .outerRadius(radius)
+        )
+        .attr('fill', function(d){ return(color(d.data.key)) })
+        .attr("stroke", "black")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.58)
+}
+
+
+// Function to walk the bugs table and gather a count of fixed and broken bugs
+function countFixedVals() {
+    let recordTable = document.getElementById('recordTable');
+    let cells = document.getElementsByClassName('bugFixed')
+
+    let count = recordTable.rows.length - 1;
+    let fixedCount = 0;
+    
+    // Gather the number of fixed bugs
+    for (let i = 0; i < cells.length; i++) {
+        try {
+            if (cells[i].firstChild.textContent == ' Yes ') {
+                fixedCount++;
+            }
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    // Calculate the number of broken bugs
+    let brokenCount = count - fixedCount;
+
+    let fixedObj = {fixed: fixedCount, broken: brokenCount, count: count};
+    return fixedObj;
+}
+
+
+// Function to create the chart on initial page load
+function initChart() {
+    // Call the function to crawl the table
+    let fixedObj = countFixedVals();
+
+    console.log("count: " + fixedObj.count);
+    console.log("fixed: " + fixedObj.fixed);
+    console.log("broken: " + fixedObj.broken);
+
+    printChart(fixedObj.fixed, fixedObj.broken, fixedObj.count);
+}
+
+
+// Function to update the chart on database reset
+function updateChartReset() {
+    let count = 20;
+    let fixedCount = 2;
+    let brokenCount = 18;
+
+    console.log("count: " + count);
+    console.log("fixed: " + fixedCount);
+    console.log("broken: " + brokenCount);
+
+    // Clear the chart and then print it
+    let chartDiv = document.getElementById('my_dataviz');
+    chartDiv.innerHTML = '';
+    printChart(fixedCount, brokenCount, count);
+}
+
+
+// Function to update the chart when deleting a bug
+function updateChartDelete() {
+    // Call the function to crawl the table
+    let fixedObj = countFixedVals();
+
+    console.log("count: " + fixedObj.count);
+    console.log("fixed: " + fixedObj.fixed);
+    console.log("broken: " + fixedObj.broken);
+
+
+    // Clear the chart and then print it
+    let chartDiv = document.getElementById('my_dataviz');
+    chartDiv.innerHTML = '';
+    printChart(fixedObj.fixed, fixedObj.broken, fixedObj.count);
+}
+
+
+// Function to update the chart when adding a bug
+function updateChartAdd() {
+    // Call the function to crawl the table
+    let fixedObj = countFixedVals();
+
+    console.log("count: " + fixedObj.count);
+    console.log("fixed: " + fixedObj.fixed);
+    console.log("broken: " + fixedObj.broken);
+
+
+    // Clear the chart and then print it
+    let chartDiv = document.getElementById('my_dataviz');
+    chartDiv.innerHTML = '';
+    printChart(fixedObj.fixed, fixedObj.broken, fixedObj.count);
+}
+
+
+// Initial call to create and print the chart
+initChart();
